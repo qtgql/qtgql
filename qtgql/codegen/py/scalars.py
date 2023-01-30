3b1714fb57c92fbb0dee29617286b1943c118ab3
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-01-30 21:11:30 +0200

    doc: added docs.

--- qtgql/codegen/py/scalars.py
+++ qtgql/codegen/py/scalars.py
@@ -1,26 +1,44 @@
 from datetime import datetime
-from typing import Any, Optional, Type, TypeVar
+from typing import Any, Generic, Optional, Type, TypeVar
 
 T = TypeVar("T")
 
+__all__ = ["BaseCustomScalar", "DateTimeScalar", "BuiltinScalars", "CUSTOM_SCALARS"]
 
-class BaseCustomScalar:
+
+class BaseCustomScalar(Generic[T]):
+    """Class to extend by user defined scalars."""
+
     __slots__ = "_value"
     GRAPHQL_NAME: str
+    """The *real* GraphQL name of the scalar (used by the codegen inspection
+    pipeline)."""
     DEFAULT_DESERIALIZED: Any = ""
+    """A place holder graphql query returned null or the field wasn't
+    queried."""
 
-    def __init__(self, v: Optional[Any] = None):
+    def __init__(self, v: Optional[T] = None):
         self._value = v
 
     @classmethod
     def from_graphql(cls, v: Optional[Any] = None) -> "BaseCustomScalar":
+        """Deserializes data fetched from graphql, This is useful when you want
+        to set a first-of value that will later be used by `to_qt()`.
+
+        **must be overridden**!
+        """
         raise NotImplementedError
 
     def to_qt(self) -> Any:
+        """Will be used by the property getter, This is the official value that
+        Qt should "understand".
+
+        **must be overridden**!
+        """
         raise NotImplementedError
 
 
-class DateTimeScalar(BaseCustomScalar):
+class DateTimeScalar(BaseCustomScalar[datetime]):
     GRAPHQL_NAME: str = "DateTime"
     DEFAULT_DESERIALIZED = " --- "
 
--- qtgql/typingref.py
+++ qtgql/typingref.py
@@ -1,5 +1,5 @@
 import inspect
-from typing import Any, Generic, List, Optional, Type, Union, get_args, get_origin
+from typing import Any, List, Optional, Type, Union, get_args, get_origin
 
 from attrs import define
 
@@ -95,7 +95,9 @@ class TypeHinter:
         return self.type is Optional
 
     def is_generic(self) -> bool:
-        return inspect.isclass(self.type) and get_origin(self.type) is Generic
+        return inspect.isclass(self.type) and "Generic.__class_getitem__" in repr(
+            self.type.__class_getitem__
+        )
 
     def is_list(self) -> bool:
         return self.type in (list, List)
