7c0f40b19ee99b08e5b98c6474fad96b9d428886
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-02-14 15:10:26 +0200

    nit: codecov fixes.

--- qtgql/codegen/py/runtime/environment.py
+++ qtgql/codegen/py/runtime/environment.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 from qtgql.gqltransport.client import GqlWsTransportClient
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from qtgql.codegen.py.runtime.queryhandler import BaseQueryHandler
 
 
@@ -20,20 +20,9 @@ class QtGqlEnvironment:
         self.client = client
         self.query_handlers: dict[int, BaseQueryHandler] = {}
 
-    def register_query(self, handler: BaseQueryHandler):
-        self.query_handlers[hash(handler)] = handler
-
     @classmethod
     def from_url(cls, url: str):
         return cls(client=GqlWsTransportClient(url=url))
-
-    def get_handler_by_query(self, query_id: int) -> BaseQueryHandler:
-        return self.query_handlers[query_id]
-
-    def fetch_all(self):
-        """Fetches all registered query handlers."""
-        for query in self.query_handlers.values():
-            query.fetch()
 
 
 ENV_MAP: dict[str, QtGqlEnvironment] = {}
