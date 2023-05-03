40b70bc2d396d9d83d5b6f154793dfdac29c32e6
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-05-03 15:13:47 +0300

    added tests for qgraphqllist

--- qtgql/bases/qgraphqllistmodel.hpp
+++ qtgql/bases/qgraphqllistmodel.hpp
@@ -21,7 +21,6 @@ class _QGraphQLListModelMixin : public QAbstractListModel {
   }
 
  protected:
-  static const int c_object_role = Qt::UserRole + 1;
   const QHash<int, QByteArray> c_role_names = default_roles();
   static const QModelIndex& invalid_index() {
     static const QModelIndex ret = QModelIndex();
@@ -43,6 +42,7 @@ class _QGraphQLListModelMixin : public QAbstractListModel {
   int rowCount(const QModelIndex& parent = QModelIndex()) const override {
     return (!parent.isValid() ? m_count : 0);
   }
+  static const int QOBJECT_ROLE = Qt::UserRole + 1;
   QHash<int, QByteArray> roleNames() const override { return c_role_names; }
 
  signals:
@@ -65,50 +65,42 @@ class QGraphQLListModelABC : public _QGraphQLListModelMixin {
 
  protected:
   T_sharedObjectQlist m_data;
-  int rowCount(const QModelIndex& parent = QModelIndex()) const override final {
-    return (!parent.isValid() ? m_count : 0);
+
+  void insert_common(const int from, const int to, std::function<void()> func) {
+    beginInsertRows(invalid_index(), from, to);
+    func();
+    update_count();
+    endInsertRows();
+  }
+  void remove_common(const int from, const int to, std::function<void()> func) {
+    beginRemoveRows(invalid_index(), from, to);
+    func();
+    update_count();
+    endRemoveRows();
   }
 
  public:
   explicit QGraphQLListModelABC(
       QObject* parent = nullptr,
       T_sharedObjectQlist data = std::make_unique<QList<T_sharedQObject>>())
-      : _QGraphQLListModelMixin(parent), m_data{std::move(data)} {};
+      : _QGraphQLListModelMixin(parent), m_data{std::move(data)} {
+    m_count = m_data->length();
+  };
 
   QVariant data(const QModelIndex& index, int role) const override {
     auto row = index.row();
     if (row < m_count && index.isValid()) {
-      if (role == c_object_role) {
+      if (role == QOBJECT_ROLE) {
         return QVariant::fromValue(
             static_cast<QObject*>(m_data->at(row).get()));
       }
     }
     return {};
   }
-
-  void append(const T_sharedQObject& shared_obj_ref) {
-    beginInsertRows(invalid_index(), m_count, m_count);
-    m_data->append(shared_obj_ref);
-    update_count();
-    endInsertRows();
+  int rowCount(const QModelIndex& parent = QModelIndex()) const override final {
+    return m_count;
   }
 
-  void pop(const int index = -1) {
-    int real_index = (index > -1 ? index : m_count);
-    beginInsertRows(invalid_index(), m_count, m_count);
-    m_data->remove(real_index);
-    update_count();
-    endRemoveRows();
-  }
-
-  void clear() {
-    if (!m_data.isEmpty()) {
-      beginRemoveRows(invalid_index(), 0, m_count - 1);
-      m_data->clear();
-      update_count();
-      endRemoveRows();
-    }
-  }
   void insert(int index, const T_sharedQObject& shared_obj_ref) {
     if (index > m_count) {
       qWarning() << "index " << index << " is greater than count " << m_count
@@ -121,18 +113,32 @@ class QGraphQLListModelABC : public _QGraphQLListModelMixin {
       index = 0;
     }
     beginInsertRows(invalid_index(), index, index);
-    m_data->insert(index, shared_obj_ref);
-    update_count();
-    endInsertRows();
+    insert_common(index, index, [&] { m_data->insert(index, shared_obj_ref); });
+  }
+
+  void append(const T_sharedQObject& shared_obj_ref) {
+    insert_common(m_count, m_count,
+                  [&]() -> void { m_data->append(shared_obj_ref); });
+  }
+
+  void pop(const int index = -1) {
+    int real_index = (index > -1 ? index : m_count);
+    remove_common(real_index, real_index, [&] { m_data->remove(real_index); });
+  }
+
+  void clear() {
+    if (!m_data.isEmpty()) {
+      remove_common(0, m_count - 1, [&] {
+        m_data->clear();
+        update_count();
+      });
+    }
   }
 
   bool removeRows(int row, int count,
                   const QModelIndex& parent = QModelIndex()) override {
     if ((row + count) <= m_count) {
-      beginRemoveRows(invalid_index(), row, count);
-      m_data->remove(row, count);
-      update_count();
-      endRemoveColumns();
+      remove_common(row, count, [&] { m_data->remove(row, count); });
       return true;
     }
     return false;
--- tests/test_qgraphqllistmodel/test_qgraphqllistmodel.cpp
+++ tests/test_qgraphqllistmodel/test_qgraphqllistmodel.cpp
@@ -1,17 +1,50 @@
+#include <QSignalSpy>
 #include <QTest>
 #include <catch2/catch_test_macros.hpp>
 
+#include "fooqobject.hpp"
 #include "qgraphqllistmodel.hpp"
 
-class SampleQGraphQLListModelABC
-    : public qtgql::QGraphQLListModelABC<std::shared_ptr<QObject>> {
+typedef std::shared_ptr<FooQObject> SharedFoo;
+
+class SampleQGraphQLListModel : public qtgql::QGraphQLListModelABC<SharedFoo> {
   void update(const QList<QJsonObject>& data,
               const SelectionsConfig& selections) override {
     std::ignore = data;
     std::ignore = selections;
   }
+  using qtgql::QGraphQLListModelABC<SharedFoo>::QGraphQLListModelABC;
 };
 
-TEST_CASE("default QGraphQLListModelABC modifications and operations") {
-  auto a = SampleQGraphQLListModelABC{};
+TEST_CASE("default QGraphQLListModelABC modifications and operations",
+          "[qgraphqllistmodle-raw]") {
+  std::unique_ptr<std::vector<int>> a = std::make_unique<std::vector<int>>();
+  std::unique_ptr<QList<SharedFoo>> init_list =
+      std::make_unique<QList<SharedFoo>>();
+  QList<SharedFoo> init_list_copy;
+  for (const auto& a : {"foo", "bar", "baz"}) {
+    auto obj = std::make_shared<FooQObject>(a);
+    init_list->append(obj);
+    init_list_copy.append(obj);
+  }
+  auto model_with_data = SampleQGraphQLListModel{nullptr, std::move(init_list)};
+
+  SECTION("object role is USER_ROLE + 1") {
+    int expected = Qt::UserRole + 1;
+    auto roles = model_with_data.roleNames();
+    REQUIRE(roles.value(expected) == QByteArray("qtObject"));
+  }
+  SECTION("test row count") {
+    REQUIRE(model_with_data.rowCount() == init_list_copy.length());
+  }
+
+  SECTION("test returns data") {
+    auto res = model_with_data.data(model_with_data.index(0),
+                                    SampleQGraphQLListModel::QOBJECT_ROLE);
+    REQUIRE(res.canConvert<FooQObject>());
+    auto v = res.value<FooQObject*>();
+    REQUIRE(v->val == init_list_copy.value(0)->val);
+  }
+
+  SECTION("test pop") { QSignalSpy spy(box, SIGNAL(clicked(bool))); }
 }
