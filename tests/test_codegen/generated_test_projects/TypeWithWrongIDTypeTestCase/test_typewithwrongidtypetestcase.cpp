5697d40950378430af1c2e16ed78ccb3519ddc62
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-05-15 14:46:01 +0300

    m13 - work on passing tests from python

--- tests/test_codegen/generated_test_projects/TypeWithWrongIDTypeTestCase/test_typewithwrongidtypetestcase.cpp
+++ tests/test_codegen/generated_test_projects/TypeWithWrongIDTypeTestCase/test_typewithwrongidtypetestcase.cpp
@@ -3,6 +3,7 @@
 #include <catch2/catch_test_macros.hpp>
 
 #include "debugableclient.hpp"
+#include "graphql/__generated__/MainQuery.hpp"
 
 TEST_CASE("TypeWithWrongIDTypeTestCase") {
   auto addr = get_server_address("97455992");
@@ -13,6 +14,9 @@ TEST_CASE("TypeWithWrongIDTypeTestCase") {
       std::make_shared<qtgql::QtGqlEnvironment>(
           "TypeWithWrongIDTypeTestCase",
           std::unique_ptr<qtgql::GqlWsTransportClient>(client)));
+  auto mq = std::make_shared<mainquery::MainQuery>();
+  mq->fetch();
+  REQUIRE(QTest::qWaitFor([&]() -> bool { return mq->completed(); }, 1500));
 
-  REQUIRE(false);
+  REQUIRE(mq->get_data()->get_id() != qtgql::CONSTANTS::ID);
 }
