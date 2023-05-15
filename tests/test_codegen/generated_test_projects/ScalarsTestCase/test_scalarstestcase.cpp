6cb3d9b206fcd34fc27cb9027b1c729f60828726
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-05-15 11:28:24 +0300

    ms8 - first generated testcase pass!!!

--- tests/test_codegen/generated_test_projects/ScalarsTestCase/test_scalarstestcase.cpp
+++ tests/test_codegen/generated_test_projects/ScalarsTestCase/test_scalarstestcase.cpp
@@ -5,11 +5,18 @@
 #include "graphql/__generated__/MainQuery.hpp"
 
 TEST_CASE("ScalarsTestCase") {
+  auto client = new qtgql::GqlWsTransportClient(
+      {.url = {"ws://localhost:9000/91874716"}});
+  REQUIRE(QTest::qWaitFor([&]() -> bool { return client->is_valid(); }, 1500));
+
   qtgql::QtGqlEnvironment::set_gql_env(
       std::make_shared<qtgql::QtGqlEnvironment>(
-          "ScalarsTestCase", std::unique_ptr<qtgql::GqlWsTransportClient>(
-                                 new qtgql::GqlWsTransportClient(
-                                     {.url = {"127.0.0.1:9000/91874716"}}))));
-  REQUIRE(false);
+          "ScalarsTestCase",
+          std::unique_ptr<qtgql::GqlWsTransportClient>(client)));
+
   auto mq = std::make_shared<mainquery::MainQuery>();
+  mq->fetch();
+  REQUIRE(QTest::qWaitFor([&]() -> bool { return mq->completed(); }, 1500));
+
+  delete client;
 }
