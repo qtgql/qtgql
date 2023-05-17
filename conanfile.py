30756e95c53af18af9fb08a0abf0b3cb494c9b25
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-05-17 16:41:25 +0300

    first conan build cmake

---
+++ conanfile.py
@@ -0,0 +1,93 @@
+import subprocess
+from functools import cached_property
+from pathlib import Path
+
+from conan import ConanFile
+from conan.tools.cmake import CMake
+from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMakeToolchain
+from conan.tools.scm import Git
+
+ConanBool = [True, False]
+
+
+def get_version_from_poetry():
+    res = subprocess.run("poetry version".split(" "), capture_output=True)
+    return res.stdout.decode().replace("qtgql ", "").rstrip()
+
+
+class QtGqlRecipe(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    name = "qtgql"
+    license = "MIT"
+    author = "Nir Benlulu nrbnlulu@gmail.com"
+    url = "https://github.com/qtgql/qtgql"
+    description = "GraphQL codegen client library for Qt"
+    topics = ("GraphQL", "Qt", "codegen")
+    version = get_version_from_poetry()
+
+    options = {"qt_version": ["6.5.0"], "verbose": ConanBool, "testing": ConanBool}
+    default_options = {
+        "verbose": False,
+        "testing": False,
+    }
+
+    def source(self) -> None:
+        git = Git(self)
+        git.clone("https://github.com/qtgql/qtgql.git", target=".")
+        git.checkout("migrate_to_cpp")
+
+    def requirements(self) -> None:
+        ...
+
+    def build_requirements(self) -> None:
+        self.tool_requires("cmake/3.22.6")
+
+    def layout(self) -> None:
+        cmake_layout(self)
+
+    @cached_property
+    def aqt_install_dir(self) -> Path:
+        ret = Path.home() / "MyConnandeps" / "Qt"
+        if not ret.exists():
+            ret.mkdir(parents=True)
+        return ret
+
+    @property
+    def qt_arch(self) -> str:
+        os_name = self.settings.os.value.lower()
+        if os_name == "linux":
+            return "gcc_64"
+        return self.settings.arch.value
+
+    @cached_property
+    def qt6_install_dir(self):
+        qt_version = "6.5.0"
+        return self.aqt_install_dir / qt_version / self.qt_arch / "lib" / "cmake" / "Qt6"
+
+    def generate(self) -> None:
+        qt_version = "6.5.0"
+        os_name = self.settings.os.value.lower()
+        if not self.qt6_install_dir.exists():
+            subprocess.run(
+                f"aqt install-qt {os_name} desktop {qt_version} {self.qt_arch} --outputdir {str(self.aqt_install_dir)} -m qtwebsockets".split(
+                    " ",
+                ),
+            )
+        tc = CMakeToolchain(self)
+        tc.cache_variables["Qt6_DIR"] = str(self.qt6_install_dir)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+
+#
+# if __name__ == "__main__":
+#     rec
