76e7870a3f62a13cbec6ef239423e0f7c6309150
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-05-18 10:24:42 +0300

    fixes for windows

--- tests/test_codegen/templates_for_tests/configtemplate.jinja.py
+++ tests/test_codegen/templates_for_tests/configtemplate.jinja.py
@@ -3,6 +3,6 @@ from pathlib import Path
 from qtgqlcodegen.config import QtGqlConfig
 
 config = QtGqlConfig(
-    graphql_dir=Path("👉 context.config.graphql_dir 👈"),
+    graphql_dir=Path(r"👉 context.config.graphql_dir 👈"),
     env_name="👉 context.config.env_name 👈",
 )
