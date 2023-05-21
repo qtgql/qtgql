93d2d3836349571d43b92a93d794a70736bd64d2
ניר <nrbnlulu@gmail.com>
2023-05-21 22:03:27 +0300

    first succesful build for windows

--- conanfile.py
+++ conanfile.py
@@ -107,7 +107,6 @@ class QtGqlRecipe(ConanFile):
             "binaryDir"
         ] = PATHS.QTGQL_TEST_TARGET.as_posix()  # cmake works with posix paths only
         tc.variables["QTGQL_TESTING"] = self.should_test
-        tc.cache_variables["Qt6_DIR"] = str(self.qt6_install_dir)   
         tc.generate()
 
     def build(self):
