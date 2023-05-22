c6ff4517dfc0df66009db86c3fdf7d1440bacc0a
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-05-22 14:45:37 +0300

    add comment workflow

--- .github/workflows/scripts/ghub.py
+++ .github/workflows/scripts/ghub.py
@@ -7,9 +7,9 @@ def get_env(name: str) -> str:
     return os.environ[name]
 
 
-g = Github(login_or_token=get_env("__BOT_TOKEN__"), base_url="https://github.com/api/v3")
+g = Github(login_or_token=get_env("BOT_TOKEN"), base_url="https://github.com/api/v3")
 qtgql = g.get_repo("qtgql/qtgql")
 
 
 def get_current_pr():
-    return qtgql.get_pull(int(get_env("__PR_NUMBER__")))
+    return qtgql.get_pull(int(get_env("PR_NUMBER")))
