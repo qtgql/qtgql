d9ee3b1328a3e7a6183cfd09d7f6758a44c7f4a5
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-05-24 15:29:38 +0300

    moved bases into namespace, pass.

---
+++ qtgql/bases/inc/qtgql/bases/bases.hpp
@@ -0,0 +1,7 @@
+#pragma once
+#include "../../../constants.hpp"
+#include "../../../environment.hpp"
+#include "../../../listmodel.hpp"
+#include "../../../metadata.hpp"
+#include "../../../networklayer.hpp"
+#include "../../../objecttype.hpp"
