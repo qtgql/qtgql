c8f12563c4b2aa392dbfc388f6d25a82598c3a19
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-05-29 20:09:26 +0300

    update release workflow.

--- .github/workflows/scripts/testcase_impl_status.py
+++ .github/workflows/scripts/testcase_impl_status.py
@@ -8,6 +8,7 @@ import jinja2
 from attr import define
 
 from .ghub import get_current_pr
+from tests.conftest import PATHS
 
 if TYPE_CHECKING:
     from tests.test_codegen.testcases import QGQLObjectTestCase
@@ -19,7 +20,13 @@ template_env = jinja2.Environment(
     autoescape=jinja2.select_autoescape(),
 )
 
-TESTCASE_IMPLEMENTATION_STATUS = template_env.get_template("implementation_status.jinja.md")
+BOT_COMMENT_TEMPLATE = template_env.get_template("bot_comment.jinja.md")
+
+
+@define
+class BotCommentContext:
+    testcases_context: TstCaseImplementationStatusTemplateContext
+    release_context: ReleaseContext
 
 
 @define
@@ -34,6 +41,12 @@ class TstCaseStatus:
 
 
 @define
+class ReleaseContext:
+    success: bool
+    content: str
+
+
+@define
 class TstCaseImplementationStatusTemplateContext:
     testcases: list[TstCaseStatus]
 
@@ -43,8 +56,8 @@ class TstCaseImplementationStatusTemplateContext:
         return f"{implemented_count}/{len(self.testcases)} testcases implemented."
 
 
-def tst_case_implementation_status() -> str:
-    context = TstCaseImplementationStatusTemplateContext(
+def get_testcases_context() -> TstCaseImplementationStatusTemplateContext:
+    return TstCaseImplementationStatusTemplateContext(
         testcases=[
             TstCaseStatus(
                 test=tc,
@@ -53,18 +66,40 @@ def tst_case_implementation_status() -> str:
             for tc in all_test_cases
         ],
     )
-    return template_env.get_template("implementation_status.jinja.md").render(context=context)
 
 
-def comment_on_pr():
+release_file = PATHS.PROJECT_ROOT / "RELEASE.md"
+
+
+def get_release_file_context() -> ReleaseContext:
+    if not release_file.exists():
+        return ReleaseContext(success=False, content="")
+    else:
+        return ReleaseContext(success=True, content=release_file.read_text())
+
+
+def render(context: BotCommentContext) -> str:
+    return BOT_COMMENT_TEMPLATE.render(context=context)
+
+
+def create_or_update_bot_comment() -> None:
+    context = BotCommentContext(
+        release_context=get_release_file_context(),
+        testcases_context=get_testcases_context(),
+    )
+    content = render(context)
     pr = get_current_pr()
     for comment in pr.get_issue_comments():
         if "878ae1db-766f-49c7-a1a8-59f7be1fee8f" in comment.body:
-            comment.edit(tst_case_implementation_status())
+            comment.edit(content)
             return
 
-    pr.create_issue_comment(tst_case_implementation_status())
+    pr.create_issue_comment(content)
+
+    # fail workflow release file is not valid.
+    if not context.release_context.success:
+        raise FileNotFoundError("Could not find RELEASE.md or it is in bad format.")
 
 
 if __name__ == "__main__":
-    comment_on_pr()
+    create_or_update_bot_comment()
--- RELEASE.md
+++ RELEASE.md
@@ -2,5 +2,4 @@ Release type: patch
 
 Add support list of objects.
 When a list is deserialized the instances are mapped based on operation ID
-since lists are not part of the [field stability](https://graphql.org/learn/global-object-identification/#field-stability)
-spec.
+since lists are not part of the [field stability](https://graphql.org/learn/global-object-identification/#field-stability) spec.
