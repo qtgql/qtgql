39936b80dd20240c796b8feb8b01298244042a9d
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-06-11 13:13:55 +0300

    add mutation

--- tests/test_codegen/schemas/object_with_list_of_object.py
+++ tests/test_codegen/schemas/object_with_list_of_object.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import strawberry
 
 from tests.conftest import fake
-from tests.test_codegen.schemas.node_interface import Node
+from tests.test_codegen.schemas.node_interface import Node, NODE_DB
 
 
 @strawberry.type
@@ -13,19 +13,24 @@ class User(Node):
 
 @strawberry.type()
 class Person(Node):
-    name: str
-    age: int
+    name: str = strawberry.field(default_factory=fake.name())
+    age: int = strawberry.field(default_factory=fake.pyint())
 
 
-FRIENDS = [Person(name=fake.name(), age=fake.pyint()) for _ in range(5)]
 @strawberry.type
 class Query:
     @strawberry.field
     def user(self) -> User:
-        return User(friends=FRIENDS)
+        return User(friends=[Person() for _ in range(5)])
 
 
-
+@strawberry.type()
+class Mutation:
+    @strawberry.mutation()
+    def add_friend(self, user_id: strawberry.ID) -> User:
+        user: User = NODE_DB.get_node(user_id)
+        user.friends.append(Person())
+        return user
 
 
 schema = strawberry.Schema(query=Query)
