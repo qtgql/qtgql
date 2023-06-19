2aeb381ce3f4d4f2625a5b66d9ec5912e8875a06
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-06-19 12:07:39 +0300

    DateTimeTestCase pass.

--- tests/test_codegen/generated_test_projects/DateTimeTestCase/test_datetimetestcase.cpp
+++ tests/test_codegen/generated_test_projects/DateTimeTestCase/test_datetimetestcase.cpp
@@ -17,25 +17,26 @@ TEST_CASE("DateTimeTestCase", "[generated-testcase]") {
   mq->fetch();
   test_utils::wait_for_completion(mq);
   SECTION("test deserialize") {
-    auto d = mq->get_data();
+    auto d = mq->get_user();
     auto now = QDateTime::currentDateTime(QTimeZone::utc())
                    .toString("hh:mm (dd.mm.yyyy)");
     REQUIRE(d->get_birth() == now);
   }
   SECTION("test update and as operation variables") {
-    auto user = mq->get_data();
+    auto user = mq->get_user();
     auto modified_user_op = changeuserbirth::ChangeUserBirth::shared();
     auto new_birth = qtgql::customscalars::DateTimeScalar(
         QDateTime::currentDateTime().addDays(12));
     auto user_id = user->get_id();
-    modified_user_op->set_variables({new_birth}, user_id);
+    modified_user_op->set_variables({{new_birth}, user_id});
     auto catcher =
         test_utils::SignalCatcher({.source_obj = user, .only = "birth"});
     modified_user_op->fetch();
     REQUIRE(catcher.wait());
     test_utils::wait_for_completion(modified_user_op);
-    REQUIRE(user->get_id() == modified_user_op->get_data()->get_id());
-    REQUIRE(modified_user_op->get_data()->get_birth() == new_birth.to_qt());
+    REQUIRE(user->get_id() == modified_user_op->get_changeBirth()->get_id());
+    REQUIRE(modified_user_op->get_changeBirth()->get_birth() ==
+            new_birth.to_qt());
     REQUIRE(user->get_birth() == new_birth.to_qt());
   };
 }
