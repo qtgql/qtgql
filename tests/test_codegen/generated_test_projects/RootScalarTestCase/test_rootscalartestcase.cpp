ad9ba5f298f3e83c396850e24a90415b577adec2
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-06-25 16:18:34 +0300

    addd signal emission test.

--- tests/test_codegen/generated_test_projects/RootScalarTestCase/test_rootscalartestcase.cpp
+++ tests/test_codegen/generated_test_projects/RootScalarTestCase/test_rootscalartestcase.cpp
@@ -18,8 +18,11 @@ TEST_CASE("RootScalarTestCase", "[generated-testcase]") {
   SECTION("test deserialize") { REQUIRE(!mq->data()->get_name().isEmpty()); };
   SECTION("test update") {
     auto prev_name = mq->data()->get_name();
+    auto catcher =
+        test_utils::SignalCatcher({.source_obj = mq->data(), .only = "name"});
     auto mq2 = mainquery::MainQuery::shared();
     mq2->fetch();
+    REQUIRE(catcher.wait());
     test_utils::wait_for_completion(mq2);
     auto new_name = mq2->data()->get_name();
     REQUIRE(new_name != prev_name);
