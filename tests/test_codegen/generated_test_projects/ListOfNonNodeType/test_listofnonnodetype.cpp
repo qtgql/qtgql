62668b02adae29f5d828fb9a6512366a2fc887dc
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-07-06 14:08:37 +0300

    test update - modify attributes at index. PASS

--- tests/test_codegen/generated_test_projects/ListOfNonNodeType/test_listofnonnodetype.cpp
+++ tests/test_codegen/generated_test_projects/ListOfNonNodeType/test_listofnonnodetype.cpp
@@ -21,17 +21,20 @@ TEST_CASE("ListOfNonNodeType", "[generated-testcase]") {
     auto user = mq->data()->get_users()->first();
     REQUIRE(!user->get_name().isEmpty());
   };
-  SECTION("test update") {
-    auto change_username_mut = changeusername::ChangeUserName::shared();
+  SECTION("test update - modify attributes at index") {
     auto user = mq->data()->get_users()->first();
     auto prev_name = user->get_name();
     QString new_name("Ma");
+    REQUIRE(prev_name != new_name);
+    auto change_username_mut = changeusername::ChangeUserName::shared();
     change_username_mut->set_variables({0, new_name});
+    change_username_mut->fetch();
+    test_utils::wait_for_completion(change_username_mut);
     test_utils::SignalCatcher catcher({.source_obj = user, .only = "name"});
     mq->refetch();
     REQUIRE(catcher.wait());
     test_utils::wait_for_completion(mq);
-    REQUIRE(user->get_name() == new_name);
+    REQUIRE(user->get_name().toStdString() == new_name.toStdString());
   };
 }
 
