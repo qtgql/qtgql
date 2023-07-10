40fe66a91458819f3b43a03bf189bcaf79b6c68a
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-07-10 10:01:49 +0300

    fix regressions.

--- qtgqlcodegen/templates/macros/deserialize_concrete_field.jinja.hpp
+++ qtgqlcodegen/templates/macros/deserialize_concrete_field.jinja.hpp
@@ -22,26 +22,25 @@ auto 👉proxy_field.name👈_typename  = 👉proxy_field.name👈_data.value("_
 👉iterate_type_condition(choice,type_cond, do_on_meets, loop)👈
 {% endfor %}
 {% elif proxy_field.type.is_model -%}
-    👉proxy_field.concrete.type.member_type👈 obj_list;
+    👉proxy_field.concrete.type.member_type👈 👉proxy_field.name👈_init_list;
     for (const auto& node: data.value("👉proxy_field.name👈").toArray()){
     {% if proxy_field.type.is_model.of_type.is_queried_object_type %}
-        obj_list.append(👉 proxy_field.type.is_model.of_type.is_queried_object_type.deserializer_name 👈(node.toObject(), 👉operation_pointer👈));
+        👉proxy_field.name👈_init_list.append(👉 proxy_field.type.is_model.of_type.is_queried_object_type.deserializer_name 👈(node.toObject(), 👉operation_pointer👈));
     {% elif proxy_field.type.is_model.of_type.is_queried_union or proxy_field.type.is_model.of_type.is_queried_interface %}
         auto node_data = node.toObject();
         auto 👉proxy_field.name👈_typename = node_data.value("__typename").toString();
         {%set type_cond -%}👉proxy_field.name👈_typename{% endset -%}
         {% for choice in proxy_field.type.of_type.choices -%}
         {% set do_on_meets -%}
-        obj_list.append(👉choice.deserializer_name👈(node_data, 👉operation_pointer👈) 👉 setter_end 👈);
+        👉proxy_field.name👈_init_list.append(👉choice.deserializer_name👈(node_data, 👉operation_pointer👈) 👉 setter_end 👈);
         {% endset -%}
         👉iterate_type_condition(choice,type_cond, do_on_meets, loop)👈
         {% endfor %}
-    };
-    👉 setter_name 👈(obj_list👉 setter_end 👈);
     {% else %}
-    👉 debug_jinja(proxy_field) 👈
     throw qtgql::exceptions::NotImplementedError({"can't deserialize model of 👉proxy_field.type.of_type.__class__👈"});
     {% endif %}
+    };
+    👉 setter_name 👈(👉proxy_field.name👈_init_list👉 setter_end 👈);
 {% elif proxy_field.type.is_builtin_scalar -%}
     {% if proxy_field.type.is_void -%}
     /* deliberately empty */
