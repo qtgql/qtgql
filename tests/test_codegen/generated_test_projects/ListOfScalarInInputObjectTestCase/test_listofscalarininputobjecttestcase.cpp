8959d43ebd7c74707dbd2eab478f01fde3e16f59
Nir <88795475+nrbnlulu@users.noreply.github.com>
2023-08-03 12:55:57 +0300

    test pass.

--- tests/test_codegen/generated_test_projects/ListOfScalarInInputObjectTestCase/test_listofscalarininputobjecttestcase.cpp
+++ tests/test_codegen/generated_test_projects/ListOfScalarInInputObjectTestCase/test_listofscalarininputobjecttestcase.cpp
@@ -13,8 +13,20 @@ TEST_CASE("ListOfScalarInInputObjectTestCase", "[generated-testcase]") {
   auto env = test_utils::get_or_create_env(
       ENV_NAME, DebugClientSettings{.prod_settings = {.url = SCHEMA_ADDR}});
 
-  SECTION("test deserialize") { REQUIRE(false); };
-  SECTION("test update") { REQUIRE(false); };
+  SECTION("test deserialize") {
+    auto echo_op = echoarg::EchoArg::shared();
+    std::list<QString> echo_me = {"A", "B", "C"};
+    echo_op->set_variables({.what{{echo_me}}});
+    echo_op->fetch();
+    test_utils::wait_for_completion(echo_op);
+    auto model = echo_op->data()->get_echo();
+    REQUIRE(model->rowCount() > 0);
+    int i = 0;
+    for (const auto &expected : echo_me) {
+      REQUIRE(model->get(i) == expected);
+      i++;
+    }
+  };
 }
 
 }; // namespace ListOfScalarInInputObjectTestCase
