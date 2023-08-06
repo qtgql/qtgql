c25431b1a5a83af9ce8a0ccfdf4c1f63c84fe22e
Nir <nrbnlulu@gmail.com>
2023-08-06 17:45:28 +0300

    tests pass.

--- tests/test_codegen/schemas/list_of_input_object.py
+++ tests/test_codegen/schemas/list_of_input_object.py
@@ -1,17 +1,22 @@
 from __future__ import annotations
 
 import strawberry
+
+
 @strawberry.input()
 class Echo:
     value: str
 
+
 @strawberry.input()
 class What:
     echo: list[Echo] | None
+
+
 @strawberry.type
 class Query:
     @strawberry.field
-    def echo(self, what: What | None= None) -> list[str]:
+    def echo(self, what: What | None = None) -> list[str]:
         return [o.value for o in what.echo]
 
 
