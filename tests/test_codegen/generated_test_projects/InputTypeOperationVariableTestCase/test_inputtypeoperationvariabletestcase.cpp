a0ed9fa46710bcf566384fad2d2e8164e68381af
Nir <nrbnlulu@gmail.com>
2023-08-21 15:26:50 +0300

    migrate tests - `InputTypeOperationVariableTestCase` pass.

--- tests/test_codegen/generated_test_projects/InputTypeOperationVariableTestCase/test_inputtypeoperationvariabletestcase.cpp
+++ tests/test_codegen/generated_test_projects/InputTypeOperationVariableTestCase/test_inputtypeoperationvariabletestcase.cpp
@@ -16,8 +16,7 @@ TEST_CASE("InputTypeOperationVariableTestCase", "[generated-testcase]") {
   auto create_post = createpost::CreatePost::shared();
   QString old_post_content("this is a great post about elephants");
   create_post->set_variables(
-      {new InputTypeOperationVariableTestCase::CreatePostInput{old_post_content,
-                                                               "Post header"}});
+      {CreatePostInput::create(old_post_content, "Post header")});
   create_post->fetch();
   test_utils::wait_for_completion(create_post);
   auto post = create_post->data()->get_createPost();
@@ -29,8 +28,7 @@ TEST_CASE("InputTypeOperationVariableTestCase", "[generated-testcase]") {
     auto update_post = updatepostcontent::UpdatePostContent::shared();
     QString new_content("This is a great post about dogs");
     update_post->set_variables(
-        {new InputTypeOperationVariableTestCase::ModifyPostContentInput(
-            post->get_id(), new_content)});
+        {ModifyPostContentInput::create(post->get_id(), new_content)});
     test_utils::SignalCatcher catcher({.source_obj = post, .only = "content"});
     update_post->fetch();
     REQUIRE(catcher.wait());
