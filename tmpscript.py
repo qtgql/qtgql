14219a51dbf18913b039b210e6f16532084d5552
Nir <nrbnlulu@gmail.com>
2023-09-10 15:57:57 +0300

    shorten generated path.

--- tmpscript.py
+++ tmpscript.py
@@ -5,7 +5,7 @@ from pathlib import  Path
 import shutil
 root = Path(__file__).parent
 testcases_dir = root / "tests" / "gen"
-generated = list(testcases_dir.glob("**/qtgqlconfig.py"))
+generated = list(testcases_dir.glob("**/__generated__"))
 
 for g in generated:
-    os.remove(g)
+    shutil.rmtree(g)
