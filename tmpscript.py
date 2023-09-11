a898b1bc0fa102bd38ed71ee3cf6c62756ed2708
ניר <nrbnlulu@gmail.com>
2023-09-11 20:40:06 +0300

    add more linux deps

--- tmpscript.py
+++ tmpscript.py
@@ -1,6 +1,6 @@
 
 
-s = '"libx11-dev", "libx11-xcb-dev", "libfontenc-dev", "libice-dev", "libsm-dev", "libxau-dev", "libxaw7-dev'
+s = '"libx11-dev", "libx11-xcb-dev", "libfontenc-dev", "libice-dev", "libsm-dev", "libxau-dev", "libxaw7-dev"'
 
 l = [r.replace('"', " ") for r in s.split(", ")]
 print(" ".join(l))
