47a09c38850ebdc8763fba6c3de71ca5075af09b
ניר <nrbnlulu@gmail.com>
2023-09-11 21:59:08 +0300

    fix configs

--- scripts/run_conan.py
+++ scripts/run_conan.py
@@ -7,8 +7,12 @@ from conans.conan import run
 if __name__ == "__main__":
     TEST_PACKAGE = "conan test conan/test_package qtgql/0.119.9 --build=missing"
     CREATE = "conan create . --build=missing"
-    BUILD_TESTS = "conan build . -o test=True"
+    BUILD_TESTS = "conan build . -o test=True "
     os.environ.setdefault("NOT_ON_C3I", "1")
+    WINDOWS_CLANG = "conan build . -o test=True -s build_type=Debug --build=missing -pr=profiles/Windows_clang"
     os.chdir(Path(__file__).parent.parent)  # change working dir to root
-    sys.argv = BUILD_TESTS.split(" ")
-    sys.exit(run())
+    sys.argv = WINDOWS_CLANG.split(" ")
+    try:
+        run()
+    except BaseException as e:
+        ...
