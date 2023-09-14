16522f5f823adecc8f87c077392ee13bf047e6c1
ניר <nrbnlulu@gmail.com>
2023-09-14 23:13:37 +0300

    add source file for `SubscriptionRouter`. fix windows compilation.

--- src/qtgql/routers/detail/routers.hpp
+++ src/qtgql/routers/detail/routers.hpp
@@ -16,17 +16,9 @@ class QTGQL_EXPORT SubscriptionRouter : public bases::NetworkLayerABC {
 public:
   explicit SubscriptionRouter(
       std::shared_ptr<gqltransportws::GqlTransportWs> ws_layer,
-      std::shared_ptr<gqloverhttp::GraphQLOverHttp> http_layer)
-      : ws_layer(std::move(ws_layer)), http_layer(std::move(http_layer)) {}
-  inline void execute(const std::shared_ptr<bases::HandlerABC> &handler) final {
-    auto message = handler->message();
-    if (message.query.startsWith("subscription")) {
-      ws_layer->execute(handler);
-    } else {
-      http_layer->execute(handler);
-    }
-  }
-  ~SubscriptionRouter() = default;
+      std::shared_ptr<gqloverhttp::GraphQLOverHttp> http_layer);
+
+  void execute(const std::shared_ptr<bases::HandlerABC> &handler) override;
 };
 
 } // namespace qtgql::routers
