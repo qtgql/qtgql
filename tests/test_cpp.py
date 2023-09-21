4ce730273dfdfec9356bcae3d9f070a7f0d45def
Nir <nrbnlulu@gmail.com>
2023-09-21 18:49:16 +0300

    9 testscases per runner

--- tests/test_cpp.py
+++ tests/test_cpp.py
@@ -4,17 +4,9 @@ import os
 from typing import TYPE_CHECKING
 
 import pytest
-from conanfile import Qt6Installer
-
-from tests.conftest import IS_WINDOWS, IS_GITHUB_ACTION
 from tests.ctest_finder import collect_tests, CtestTestCommand
 if TYPE_CHECKING:
     from tests.conftest import MiniServer
-
-
-if IS_GITHUB_ACTION:
-    qt6_installer = Qt6Installer("windows" if IS_WINDOWS else "linux", "6.5.2")
-    qt6_installer.set_env_vars()
 
 
 @pytest.mark.parametrize("doctest_testcase", collect_tests(), ids=lambda v: v.test_name, )
