e476cb8e183aba4a22254db257520771b117c5a7
Nir <nrbnlulu@gmail.com>
2023-12-24 17:55:26 +0200

    test core tests only if it is the first test.

--- tests/test_codegen/generate.py
+++ tests/test_codegen/generate.py
@@ -10,9 +10,14 @@ from tests.test_codegen.testcases import (
 
 def generate_testcases_by_name(testcases_names: list[str]) -> None:
     to_gen = []
+    first_testcase = implemented_testcases[0]
     for tc in implemented_testcases:
         if tc.test_name in testcases_names:
             to_gen.append(tc)
+        if tc.test_name == first_testcase.test_name:
+            env_file = os.getenv("GITHUB_ENV")
+            with open(env_file, "a") as envfile:  # noqa: PTH123
+                envfile.write("SHOULD_TEST_CORE=1")
 
     generate_testcases(*to_gen)
 
